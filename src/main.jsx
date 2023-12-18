53d745ade6832f8191abdfc68f8668700720a3d9
abdulloh75 <abdulloxqayumov448@gmail.com>
2023-12-18 14:33:37 +0500

    complete

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { BrowserRouter } from 'react-router-dom';
 
 
-let global_url = 'http://192.168.1.139:80/api/v1/'
+let global_url = 'https://users.pythonanywhere.com/api/v1/users/'
 axios.defaults.baseURL = global_url;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
